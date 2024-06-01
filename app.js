588acab30fad63ec9c92be61fa524520b83f5fcc
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2024-06-01 23:55:51 +0530

    update

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const fs = require('fs');
-const port = 80;
+const port = process.env.PORT || 4000;
+
 
 const mongoose = require('mongoose');
 
